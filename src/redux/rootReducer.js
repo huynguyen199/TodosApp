bdba711af95a28e2765192856e1b078c71a4a9c2
Huy <khangwhat116@gmail.com>
2022-03-07 10:05:49 +0700

    Revert "Init"
    This reverts commit 1e557676bea50711fab295b92a52e4499137f912.

--- src/redux/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from 'redux';
-import TodosReducer from './Reducers/Todos.Reducer';
-
-const rootReducer = combineReducers({
-  todos: TodosReducer,
-});
-
-export default rootReducer;
